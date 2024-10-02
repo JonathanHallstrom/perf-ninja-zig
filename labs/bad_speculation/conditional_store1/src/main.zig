3d06f532330e76aa4667165f3cb86a379726b581
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-02 22:18:42 +0200

    bad_speculation1/conditional_store1

---
+++ labs/bad_speculation/conditional_store1/src/main.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+
+const oldSelect = @import("original.zig").select;
+const newSelect = @import("solution.zig").select;
+
+test {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+
+    var rng = std.Random.DefaultPrng.init(seed);
+
+    const n = 64 << 10;
+    var input: [n][2]u32 = undefined;
+    for (&input) |*e| e.* = .{
+        rng.random().uintLessThan(u32, std.math.maxInt(u32)),
+        rng.random().uintLessThan(u32, std.math.maxInt(u32)),
+    };
+
+    var out_old: [n][2]u32 = .{.{ 0, 0 }} ** n;
+    var out_new: [n][2]u32 = .{.{ 0, 0 }} ** n;
+
+    const old_len = oldSelect(n, &out_old, &input, 1 << 30, 1 << 31);
+    const new_len = newSelect(n, &out_new, &input, 1 << 30, 1 << 31);
+
+    try std.testing.expectEqualSlices([2]u32, out_old[0..old_len], out_new[0..new_len]);
+}
+
+pub fn main() !void {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+
+    var rng = std.Random.DefaultPrng.init(seed);
+
+    const n = 64 << 10;
+    var input: [n][2]u32 = undefined;
+    for (&input) |*e| e.* = .{
+        rng.random().uintLessThan(u32, std.math.maxInt(u32)),
+        rng.random().uintLessThan(u32, std.math.maxInt(u32)),
+    };
+
+    var out: [n][2]u32 = .{.{ 0, 0 }} ** n;
+    var timer = try std.time.Timer.start();
+    for (0..16 << 10) |_| {
+        const tmp = oldSelect(n, &out, &input, 1 << 30, 1 << 31);
+
+        std.mem.doNotOptimizeAway(tmp);
+        std.mem.doNotOptimizeAway(out);
+    }
+    const old_time = timer.lap();
+    for (0..16 << 10) |_| {
+        const tmp = newSelect(n, &out, &input, 1 << 30, 1 << 31);
+
+        std.mem.doNotOptimizeAway(tmp);
+        std.mem.doNotOptimizeAway(out);
+    }
+    const new_time = timer.lap();
+    const difference: i64 = @as(i64, @intCast(new_time)) - @as(i64, @intCast(old_time));
+
+    const percent = @as(f64, @floatFromInt(@abs(difference))) * 100 / @as(f64, @floatFromInt(old_time));
+
+    if (percent > 5) {
+        if (difference > 0) {
+            std.debug.print("new version is slower by: {d:.1}%\n", .{percent});
+        } else {
+            std.debug.print("new version is faster by: {d:.1}% (goal is ~20% speedup)\n", .{percent});
+        }
+    } else {
+        std.debug.print("new version is equivalent\n", .{});
+    }
+}
---
+++ labs/bad_speculation/conditional_store1/src/original.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn select(n: comptime_int, output: *[n][2]u32, input: *const [n][2]u32, lower: u32, upper: u32) usize {
+    var count: usize = 0;
+    for (input) |e| {
+        if (lower <= e[0] and e[0] <= upper) {
+            output[count] = e;
+            count += 1;
+        }
+    }
+    return count;
+}
