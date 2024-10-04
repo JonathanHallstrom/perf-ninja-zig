10c17bf25a8ba6bd91e438856b0a5880ed5ef8bd
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-04 10:58:13 +0200

    solve bad_speculation exercises

--- labs/bad_speculation/virtual_call_mispredict1/src/solution.zig
+++ labs/bad_speculation/virtual_call_mispredict1/src/solution.zig
@@ -4,8 +4,18 @@ const lib = @import("lib.zig");
 const Dynamic = lib.Dynamic;
 
 pub fn generate(arr: []Dynamic, rng: std.Random) void {
+    var counts: [3]usize = .{0} ** 3;
+    for (arr) |_| {
+        counts[rng.uintLessThan(u8, 3)] += 1;
+    }
     for (arr) |*e| {
-        e.* = Dynamic.init(rng.uintLessThan(u8, 3));
+        inline for (&counts, 0..) |*c, i| {
+            if (c.* > 0) {
+                e.* = Dynamic.init(i);
+                c.* -= 1;
+                break;
+            }
+        }
     }
 }
 
