127d6c2939e6589b52d38c37b1f31118134f9e92
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-07 19:40:11 +0200

    solve vectorization2

--- labs/core_bound/vectorization2/src/solution.zig
+++ labs/core_bound/vectorization2/src/solution.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
 
 pub fn checksum(input: []u8) u16 {
-    var res: u16 = 0;
-    for (input) |c| {
-        const sum, const carry = @addWithOverflow(res, c);
-        res = sum + carry;
+    const maximum_values = (std.math.maxInt(u32) - std.math.maxInt(u16)) / std.math.maxInt(u8);
+    var res: u32 = 0;
+    // if we process more than maximum_values at a time res might overflow
+    for (0..(input.len + maximum_values - 1) / maximum_values) |i| {
+        const start = maximum_values * i;
+        const end = @min(start + maximum_values, input.len);
+        for (input[start..end]) |c| {
+            res += c;
+        }
+        res = (res & (1 << 16) - 1) + (res >> 16);
+        res = (res & (1 << 16) - 1) + (res >> 16);
     }
-    return res;
+    return @intCast(res);
 }
