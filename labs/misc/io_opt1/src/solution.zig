76d8e7aaef41e7d4e78c1647f98d3baaa7863d67
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-07 12:25:47 +0200

    solve io_opt1

--- labs/misc/io_opt1/src/solution.zig
+++ labs/misc/io_opt1/src/solution.zig
@@ -3,10 +3,12 @@ const std = @import("std");
 pub fn getCrc32(reader: anytype) u32 {
     var hasher = std.hash.Crc32.init();
 
-    while (reader.readByte()) |byte| {
-        hasher.update(&.{byte});
-    } else |_| {
-        // got EOF
+    var buf: [1 << 21]u8 = undefined;
+    while (reader.read(&buf)) |amt_read| {
+        if (amt_read == 0) break;
+        hasher.update(buf[0..amt_read]);
+    } else |e| {
+        std.debug.panic("error: {}\n", .{e});
     }
 
     return hasher.final();
