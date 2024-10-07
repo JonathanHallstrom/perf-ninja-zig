f5789b85d518e3fb0c3dff292dfc82d675786c46
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-07 19:44:11 +0200

    add alternate solution to vectorization2

--- labs/core_bound/vectorization2/src/solution.zig
+++ labs/core_bound/vectorization2/src/solution.zig
@@ -14,4 +14,29 @@ pub fn checksum(input: []u8) u16 {
         res = (res & (1 << 16) - 1) + (res >> 16);
     }
     return @intCast(res);
+
+    // note: you can get almost the same speedup unrolling manually
+    // var res: u16 = 0;
+    // var i: usize = 0;
+
+    // if (std.simd.suggestVectorLength(u16)) |unroll|{
+    //     var accs: @Vector(unroll, u16) = @splat(0);
+    //     while (i + unroll - 1 < input.len) : (i += unroll) {
+    //         const vals: @Vector(unroll, u16) = input[i..][0..unroll].*;
+    //         accs +%= vals;
+    //         const zeroes: @Vector(unroll, u16) = @splat(0);
+    //         const ones: @Vector(unroll, u16) = @splat(1);
+    //         accs +%= @select(u16, accs < vals, ones, zeroes);
+    //     }
+    //     for (@as([unroll]u16, accs)) |acc| {
+    //         res +%= acc;
+    //         res += @intFromBool(res < acc);
+    //     }
+    // }
+
+    // for (input[i..]) |c| {
+    //     const sum, const carry = @addWithOverflow(res, c);
+    //     res = sum + carry;
+    // }
+    // return res;
 }
