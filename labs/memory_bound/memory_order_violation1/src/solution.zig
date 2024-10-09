cce046b938f0b2e35f7c055e1e82f97245ba0006
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-09 17:22:45 +0200

    solve memory_order_violation1

--- labs/memory_bound/memory_order_violation1/src/solution.zig
+++ labs/memory_bound/memory_order_violation1/src/solution.zig
@@ -1,7 +1,14 @@
 const std = @import("std");
 
 pub fn histogram(image: []const u8) [256]u32 {
-    var res: [256]u32 = .{0} ** 256;
-    for (image) |pixel| res[pixel] += 1;
-    return res;
+    const unroll = 6;
+    var accumulators: [unroll]@Vector(256, u32) = undefined;
+    @memset(std.mem.asBytes(&accumulators), 0);
+    var i: usize = 0;
+    while (i + unroll - 1 < image.len) : (i += unroll) {
+        for (0..unroll) |j| accumulators[j][image[i + j]] += 1;
+    }
+    for (image[i..]) |pixel| accumulators[0][pixel] += 1;
+    for (1..unroll) |j| accumulators[0] += accumulators[j];
+    return accumulators[0];
 }
