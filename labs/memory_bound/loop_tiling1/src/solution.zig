1dbe748cc9b92c4be6dbced13e03c452e357ba50
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-29 09:32:33 +0100

    solve loop_tiling1

--- labs/memory_bound/loop_tiling1/src/solution.zig
+++ labs/memory_bound/loop_tiling1/src/solution.zig
@@ -7,9 +7,24 @@ const identityMatrix = lib.identityMatrix;
 
 // main focus is this
 pub fn transpose(out: *Matrix, in: *const Matrix) void {
-    for (0..N) |i| {
-        for (0..N) |j| {
-            out[i][j] = in[j][i];
+    const incr = 64;
+    var lo_i: usize = 0;
+    var hi_i: usize = 0;
+    while (lo_i < N) : ({
+        lo_i = hi_i;
+        hi_i = @min(hi_i + incr, N);
+    }) {
+        var lo_j: usize = 0;
+        var hi_j: usize = 0;
+        while (lo_j < N) : ({
+            lo_j = hi_j;
+            hi_j = @min(hi_j + incr, N);
+        }) {
+            for (lo_i..hi_i) |i| {
+                for (lo_j..hi_j) |j| {
+                    out[i][j] = in[j][i];
+                }
+            }
         }
     }
 }
