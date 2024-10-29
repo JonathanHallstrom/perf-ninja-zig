d474e8a19aa4361a952dba436124f86fdde791bb
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-29 09:33:41 +0100

    remove pointless identity matrix

--- labs/memory_bound/loop_tiling1/src/solution.zig
+++ labs/memory_bound/loop_tiling1/src/solution.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 const lib = @import("lib.zig");
 const N = lib.N;
 const Matrix = lib.Matrix;
-const identityMatrix = lib.identityMatrix;
 
 // main focus is this
 pub fn transpose(out: *Matrix, in: *const Matrix) void {
